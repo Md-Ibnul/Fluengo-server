95afe32b5d01c3cfd3e6da189fd5d5bc049d2d8d
Md-Ibnul <ibnulhasan456@gmail.com>
2023-06-08 19:43:05 +0600

    add class to db

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(express.json())
 app.use(morgan('dev'))
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3rhi256.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -34,7 +34,8 @@ async function run() {
     const usersCollection = client.db('fluengodb').collection('users')
     const classesCollection = client.db('fluengodb').collection('classes')
     const selectClassCollection = client.db('aircncDb').collection('selectClass')
-
+    
+// User related API
     // Save user Email and role im DB
     app.put('/users/:email', async(req, res) => {
         const email = req.params.email;
@@ -48,14 +49,46 @@ async function run() {
         res.send(result);
     })
 
-    // Get user
-    app.get('/users/:email', async(req, res) => {
-        const email = req.params.email;
-        const query = {email: email}
-        const result = await usersCollection.findOne(query)
-        res.send(result);
+    // Get all users
+    app.get('/users', async(req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    })
+
+    // make admin 
+    app.patch('/users/admin/:id', async(req, res) => {
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)};
+      const updateDoc = {
+        $set: {
+          role: 'Admin'
+        }
+      };
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    })
+    // make Instructor 
+    app.patch('/users/instructor/:id', async(req, res) => {
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)};
+      const updateDoc = {
+        $set: {
+          role: 'Instructor'
+        }
+      };
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
     })
     
+
+// class related API
+
+// save class to db
+app .post('/classes', async(req, res) => {
+  const cls =req.body;
+  const result = await classesCollection.insertOne(cls)
+  res.send(result)
+})
 
     await client.connect();
     // Send a ping to confirm a successful connection
